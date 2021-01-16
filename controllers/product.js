f33fba60c107f866ccdb92cc57311e2af19752b7
DanielTndj <daniel.tndj@gmail.com>
2021-01-16 23:01:45 +0700

    Added route for update product

--- controllers/product.js
+++ controllers/product.js
@@ -22,3 +22,53 @@ exports.listAll = async (req, res) => {
     .exec();
   res.json(products);
 };
+
+exports.remove = async (req, res) => {
+  try {
+    const { slug } = req.params;
+    const deletedProduct = await Product.findOneAndRemove({ slug }).exec();
+
+    res.json(deletedProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Category delete failed");
+  }
+};
+
+exports.read = async (req, res) => {
+  try {
+    const product = await Product.findOne({ slug: req.params.slug })
+      .populate("category")
+      .populate("subs")
+      .exec();
+
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.update = async (req, res) => {
+  let { title } = req.body;
+
+  try {
+    if (title) {
+      req.body.slug = slugify(title);
+    }
+
+    const updated = await Product.findOneAndUpdate(
+      { slug: req.params.slug },
+      req.body,
+      {
+        new: true,
+      }
+    ).exec();
+
+    res.json(updated);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      err: err.message
+    })
+  }
+};
--- routes/product.js
+++ routes/product.js
@@ -5,9 +5,12 @@ const router = express.Router();
 const { authCheck, adminCheck } = require("../middlewares/auth");
 
 // controllers
-const { create, listAll } = require("../controllers/product");
+const { create, listAll, remove, read, update } = require("../controllers/product");
 
 router.post("/product", authCheck, adminCheck, create);
 router.get("/products/:count", listAll);
+router.delete("/product/:slug", authCheck, adminCheck, remove);
+router.get("/product/:slug", read);
+router.put("/product/:slug", authCheck, adminCheck, update);
 
 module.exports = router;
