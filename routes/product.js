d9d7b4dcf368ced249e8a22c2b956ec2b5a81698
DanielTndj <daniel.tndj@gmail.com>
2021-01-17 20:35:28 +0700

    Added func to get related product

--- routes/product.js
+++ routes/product.js
@@ -14,6 +14,7 @@ const {
   list,
   productsCount,
   productStar,
+  listRelated,
 } = require("../controllers/product");
 
 router.post("/product", authCheck, adminCheck, create);
@@ -26,5 +27,8 @@ router.post("/products", list);
 
 // rating
 router.put("/product/star/:productId", authCheck, productStar);
+
+// related
+router.get("/product/related/:productId", listRelated);
 
 module.exports = router;
