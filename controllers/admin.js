48e8c04670f34b9861bfab13b51579d3b560bb91
DanielTndj <daniel.tndj@gmail.com>
2021-01-24 01:33:57 +0700

    Added nodemailer to send status order

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,14 @@
 const Order = require("../models/order");
+const User = require("../models/user");
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "mebelmurah99@gmail.com",
+    pass: "cob4cob4",
+  },
+});
 
 exports.orders = async (req, res) => {
   let allOrders = await Order.find({})
@@ -10,13 +20,32 @@ exports.orders = async (req, res) => {
 };
 
 exports.orderStatus = async (req, res) => {
-  const { orderId, orderStatus } = req.body;
+  const { orderId, orderStatus, orderedBy } = req.body;
 
   let updated = await Order.findByIdAndUpdate(
     orderId,
     { orderStatus },
     { new: true }
   ).exec();
+
+  //get email from req.body
+  const userEmail = await User.findOne({ _id: orderedBy })
+    .select("email")
+    .exec();
+
+  console.log("USER EMAIL", userEmail);
+
+  const mailOptions = {
+    from: "mebelmurah99@gmail.com",
+    to: userEmail.email,
+    subject: "Your Order Status",
+    text: `Your order status is ${orderStatus}. Thank you for ordering in our shop :)`,
+  };
+
+  transporter.sendMail(mailOptions, (err, success) => {
+    if (err) throw err;
+    console.log("Email sent:" + info.response);
+  });
 
   res.json(updated);
 };
